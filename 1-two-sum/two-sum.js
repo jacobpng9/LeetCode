40fa2cbdd3b8818b32893f69eacf3dc2aac82625
Jacob <110934976+jacobng24@users.noreply.github.com>
2024-02-06 12:43:16 -0700

    Time: 107 ms (26.31%) | Memory: 49 MB (30.12%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = i+1; j < nums.length; j++) {
+            sum = nums[i] + nums[j]
+            if (sum == target) {
+                return ([i, j]);
+            }
+        }
+    }
+};
