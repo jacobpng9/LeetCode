7da0c72c0a2fe1f6262f6220a4288bf31dc147df
Jacob <110934976+jacobng24@users.noreply.github.com>
2024-02-06 18:55:02 -0700

    Time: 55 ms (47.47%) | Memory: 48.9 MB (20.36%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -4,10 +4,14 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    for (i=0;i<nums.length;i++) {
-        if (nums[i] == val) {
-            nums.splice(i, 1);
-            i-=1;
+     let k = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
         }
     }
+
+    return k;
 };
