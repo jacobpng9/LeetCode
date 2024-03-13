cf7709c00510c64fea16a6e70fd05778a82ef7ca
Jacob <110934976+jacobng24@users.noreply.github.com>
2024-03-13 12:17:11 -0700

    Time: 57 ms (31.60%) | Memory: 49.5 MB (12.52%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    let position = haystack.search(needle)
+    /* for (let i = 0; i < haystack.length; i++) {
+        if (haystack)
+    } */
+    return position;
+};
